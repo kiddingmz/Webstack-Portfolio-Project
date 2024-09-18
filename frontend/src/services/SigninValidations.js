c98874a18926682d75035f4429a2d3371903b13a
kiddingmz <bata.isidro@gmail.com>
2024-09-18 11:37:01 +0200

    CHORE: improves the sign in validations

--- frontend/src/services/SigninValidations.js
+++ frontend/src/services/SigninValidations.js
@@ -11,12 +11,12 @@ export default class SigninValidations {
 
         if (!Validations.checkEmail(this.email)) {
             // errors.push("Endereço de email inválido");
-            errors['email'] = "Endereço de email inválido";
+            errors['email'] = "Invalid email address";
         }
 
         if (!Validations.minLength(this.password, 6)) {
             // errors.push("A senha deve ter pelo menos 6 caracteres");
-            errors['password'] = "A senha deve ter pelo menos 6 caracteres";
+            errors['password'] = "Password must have at least 6 characters";
         }
 
         return errors;
