3050a17167b7febef2d47cb151788a408d439d6d
kiddingmz <bata.isidro@gmail.com>
2024-09-18 11:37:45 +0200

    CHORE: improves the sign up validations

--- frontend/src/services/SignupValidations.js
+++ frontend/src/services/SignupValidations.js
@@ -11,17 +11,14 @@ export default class SignupValidations {
         let errors = [];
 
         if (!Validations.checkEmail(this.email)) {
-            // errors.push("Invalid email address");
             errors['email'] = "Invalid email address";
         }
 
         if (!Validations.minLength(this.name, 3)) {
-            // errors.push("Name must be at least 3 characters");
             errors['name'] = "Name must be at least 3 characters";
         }
 
         if (!Validations.minLength(this.password, 6)) {
-            // errors.push("Password must be at least 6 characters");
             errors['password'] = "Password must be at least 6 characters";
         }
 
